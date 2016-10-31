8c6d4dce429fa99e86398cddf7abcf2aec290905
Anthony Grimes <i@raynes.me>
2016-10-31 10:15:24 -0700

    Update schemas

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS projects CASCADE;
 CREATE TABLE IF NOT EXISTS projects (
   user_id             VARCHAR(36) REFERENCES users,
   project_id          VARCHAR(36) PRIMARY KEY,
-  project_name        VARCHAR(256),
+  project_name        VARCHAR(256) NOT NULL,
   project_description TEXT
 );
 
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS session_start (
   user_id    VARCHAR(36) REFERENCES users,
   session_id VARCHAR(36) PRIMARY KEY,
   project_id VARCHAR(36) REFERENCES projects,
-  start_time TIMESTAMP,
+  start_time TIMESTAMP NOT NULL,
   session_description TEXT
 );
 
@@ -27,6 +27,6 @@ CREATE TABLE IF NOT EXISTS session_end (
   user_id       VARCHAR(36) REFERENCES users,
   session_id    VARCHAR(36) REFERENCES session_start,
   project_id    VARCHAR(36) REFERENCES projects,
-  end_time      TIMESTAMP,
-  total_seconds INT
+  end_time      TIMESTAMP NOT NULL,
+  total_seconds INT NOT NULL
 );
